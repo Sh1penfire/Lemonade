945798f487a6aacbc846580231e353a68b953c9f
Pyratite <73347888+Pyratite@users.noreply.github.com>
2021-11-30 19:23:52 +1100

    trail, attempt at squash and stretch failed

--- Assets/playerCharacter.cs
+++ Assets/playerCharacter.cs
@@ -8,10 +8,12 @@ public class playerCharacter : MonoBehaviour
     Vector2 vel = new Vector2();
     Rigidbody2D rigidbody;
     public float moveSpeed;
+    public float acceleration = 0;
     public float drag;
     public Rigidbody2D currentGrenade;
     public Vector2 grenadeMovementMultiplier;
-    private Vector2 v1 = new Vector2();
+    public float grenadeBaseThrowSpeed;
+    private Vector2 v1 = new Vector2(), v2 = new Vector2(), v3 = new Vector2();
     // Start is called before the first frame update
     void Start()
     {
@@ -24,30 +26,30 @@ public class playerCharacter : MonoBehaviour
     void Update()
     {
         vel.Set(0, 0);
-        movementMultiplier.Set(1, 1);
+        movementMultiplier.Set(acceleration, acceleration);
         bool keyDown = false;
         if (Input.GetKey(KeyCode.A))
         {
             movementMultiplier.y /= 2;
-            vel.x -= moveSpeed;
+            vel.x -= acceleration;
             keyDown = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
             movementMultiplier.y /= 2;
-            vel.x += moveSpeed;
+            vel.x += acceleration;
             keyDown = true;
         }
         if (Input.GetKey(KeyCode.S))
         {
             movementMultiplier.x /= 2;
-            vel.y -= moveSpeed;
+            vel.y -= acceleration;
             keyDown = true;
         }
         if (Input.GetKey(KeyCode.W))
         {
             movementMultiplier.x /= 2;
-            vel.y += moveSpeed;
+            vel.y += acceleration;
             keyDown = true;
         }
         vel.x *= movementMultiplier.x;
@@ -58,21 +60,38 @@ public class playerCharacter : MonoBehaviour
 
             rigidbody.AddForce(vel);
 
-            if (rigidbody.velocity.magnitude > 5)
+            if (rigidbody.velocity.magnitude > moveSpeed)
             {
                 Vector2 normalized = rigidbody.velocity.normalized;
-                normalized.x *= 5;
-                normalized.y *= 5;
+                normalized.x *= moveSpeed;
+                normalized.y *= moveSpeed;
                 rigidbody.velocity.Set(normalized.x, normalized.y);
             }
         }
 
         if (Input.GetKeyDown(KeyCode.Space)) {
+            //make the grenade
             var grenade = Instantiate(currentGrenade, transform.position, transform.rotation);
+
+            //velocity * velocity multi
             v1.Set(rigidbody.velocity.x, rigidbody.velocity.y);
             v1.Scale(grenadeMovementMultiplier);
+
+            //base grenade velocity, rotated to the player's velocity
+            v2.Set(rigidbody.velocity.x, rigidbody.velocity.y);
+            v2.Normalize();
+            v3.Set(grenadeBaseThrowSpeed, grenadeBaseThrowSpeed);
+            v2.Scale(v3);
+
+            //add together the forces, then make the grenade fly that fast
+            v1 += v2;
             grenade.AddForce(v1);
-            Debug.Log(v1);
         }
+
+        v1.Set(Mathf.Log(2, rigidbody.velocity.x) * 2 + 1, Mathf.Log(2, rigidbody.velocity.y) * 2 + 1);
+        v2.Set(v1.x - v1.y / 2, v1.y - v1.x / 2);
+        transform.localScale.Scale(v1);
+
+        Debug.Log(transform.localScale);
     }
 }
