658d69efa8a6bfd6c366c5f15c4961bdbf3e6520
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2021-11-30 17:08:17 +1100

    moved the unity things here
    unity stuf is moved over, contains the basic movement and character goes shootious

---
+++ Assets/explode.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explode : MonoBehaviour
+{
+    public ParticleSystem explosionEffect;
+    public float explosionTime;
+    public float explosionRadius;
+    private float time;
+    private Vector2 position2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position2D.x = transform.position.x;
+        position2D.y = transform.position.y;
+        time += Time.deltaTime;
+        if (time >= explosionTime) {
+            Explode();
+        }
+    }
+
+    void Explode(){
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position2D, explosionRadius, 0);
+
+        Debug.Log(position2D);
+
+        foreach (Collider2D collider in colliders){
+            Rigidbody2D rigidbody = collider.GetComponent<Rigidbody2D>();
+            if (rigidbody != null) {
+                rigidbody.AddForce(new Vector2(1, 1));
+            }
+            Debug.Log(rigidbody);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/playerCharacter.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCharacter : MonoBehaviour
+{
+    private Vector2 movementMultiplier = new Vector2(0, 0);
+    Vector2 vel = new Vector2();
+    Rigidbody2D rigidbody;
+    public float moveSpeed;
+    public float drag;
+    public Rigidbody2D currentGrenade;
+    public Vector2 grenadeMovementMultiplier;
+    private Vector2 v1 = new Vector2();
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        rigidbody.drag = drag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vel.Set(0, 0);
+        movementMultiplier.Set(1, 1);
+        bool keyDown = false;
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementMultiplier.y /= 2;
+            vel.x -= moveSpeed;
+            keyDown = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementMultiplier.y /= 2;
+            vel.x += moveSpeed;
+            keyDown = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movementMultiplier.x /= 2;
+            vel.y -= moveSpeed;
+            keyDown = true;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementMultiplier.x /= 2;
+            vel.y += moveSpeed;
+            keyDown = true;
+        }
+        vel.x *= movementMultiplier.x;
+        vel.y *= movementMultiplier.y;
+
+        if (keyDown) {
+            //float length = Mathf.Sqrt(vel.x + vel.y);
+
+            rigidbody.AddForce(vel);
+
+            if (rigidbody.velocity.magnitude > 5)
+            {
+                Vector2 normalized = rigidbody.velocity.normalized;
+                normalized.x *= 5;
+                normalized.y *= 5;
+                rigidbody.velocity.Set(normalized.x, normalized.y);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            var grenade = Instantiate(currentGrenade, transform.position, transform.rotation);
+            v1.Set(rigidbody.velocity.x, rigidbody.velocity.y);
+            v1.Scale(grenadeMovementMultiplier);
+            grenade.AddForce(v1);
+            Debug.Log(v1);
+        }
+    }
+}
